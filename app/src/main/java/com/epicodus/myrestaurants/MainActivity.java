d7d4817df3192aa8b69233f9dd137553b63a8aa1
chris young and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-04-18 09:38:20 -0700

    add intent to navigate to RestaurantsActivity

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -1,13 +1,28 @@
 package com.epicodus.myrestaurants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Interpolator;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+            mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                    startActivity(intent);
+                }
+            });
     }
 }
